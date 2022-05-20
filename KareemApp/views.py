015c874e31e67667fb86a8e3d5dec3485f55d655
mauriza22 <maurizahumaira14@gmail.com>
2022-05-20 23:34:31 +0700

    update views.py

--- KareemApp/views.py
+++ KareemApp/views.py
@@ -10,14 +10,55 @@ from KareemApp.serializers import UsersSerializers, AktivitasSerializers
 
 
 def login(request):
-    return render(request, 'login.html')
+    if request.method == 'GET':
+        return render(request, 'login.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        try:
+            user = Users.objects.get(
+                username=username, password=password)
+        except Users.DoesNotExist:
+            messages.error(request, 'Pengguna tidak ditemukan!')
+            return redirect('/login')
+
+        request.session['user_id'] = user.user_id
 
 
 def registrasi(request):
     if request.method == 'GET':
         return render(request, 'registrasi.html')
-    # elif request.method == 'POST':
-    #     return
+    elif request.method == 'POST':
+        user_data = {
+            'nama': request.POST['nama'],
+            'username': request.POST['username'],
+            'password': request.POST['password'],
+        }
+
+        password = request.POST['password']
+        konfirmasi_password = request.POST['konfirpass']
+
+        if password != konfirmasi_password:
+            messages.error(request, "Konfirmasi password tidak cocok!")
+            return redirect('/registrasi')
+
+        try:
+            user = Users.objects.get(username=user_data['username'])
+            messages.error(request, "Pengguna sudah ada!")
+            return redirect('/registrasi')
+        except:
+            pass
+
+        user_serializer = UsersSerializers(data=user_data)
+
+        if user_serializer.is_valid():
+            user_serializer.save()
+            messages.success(request, "Pendaftaran berhasil!")
+            return redirect('/login')
+
+        messages.error(request, 'Gagal menambahkan pemberi baru!')
+        return redirect('/login')
 
 
 def amalanku(request):
@@ -31,5 +72,11 @@ def landingpage(request):
 def dashboard(request):
     return render(request, 'dashboard.html')
 
+
 def foodrekom(request):
     return render(request, 'foodrekom.html')
+
+
+def logout(request):
+    logout(request)
+    return redirect('/login')
